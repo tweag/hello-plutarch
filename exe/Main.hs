5905277b08c12eead720b61c7f5dfd7e69a9072a
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2022-10-20 18:56:03 +0200

    Compile and print a basic validator

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
+import PlutusLedgerApi.V1.Scripts (Script(..))
+import PlutusCore.Pretty (displayPlcDebug)
+
 import Lib
 
 main :: IO ()
-main = sayHello "Plutarch"
+main = do
+  sayHello "Plutarch"
+  putStrLn $ displayPlcDebug $ unScript alwaysSucceeds'
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,11 +2,20 @@
 
 module Lib where
 
+import Data.Default (def)
+import Data.Either.Combinators (fromRight')
+
+import Plutarch (compile)
 import Plutarch.Prelude
 import Plutarch.Api.V1.Contexts
 import Plutarch.Api.V1.Scripts
+import PlutusLedgerApi.V1 (Script)
+
 sayHello :: String -> IO ()
 sayHello s = putStrLn $ "Hello, " <> s <> "!"
 
 alwaysSucceeds :: Term s (PAsData PDatum :--> PAsData PRedeemer :--> PAsData PScriptContext :--> PUnit)
 alwaysSucceeds = plam $ \datm redm ctx -> pconstant ()
+
+alwaysSucceeds' :: Script
+alwaysSucceeds' = fromRight' $ compile def alwaysSucceeds
