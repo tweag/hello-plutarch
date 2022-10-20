6072ee81f04461da3f22b97a03508970931db180
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2022-10-20 18:48:45 +0200

    Dumb validator that always succeeds

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,12 @@
+{-# LANGUAGE TypeOperators #-}
+
 module Lib where
 
+import Plutarch.Prelude
+import Plutarch.Api.V1.Contexts
+import Plutarch.Api.V1.Scripts
 sayHello :: String -> IO ()
 sayHello s = putStrLn $ "Hello, " <> s <> "!"
+
+alwaysSucceeds :: Term s (PAsData PDatum :--> PAsData PRedeemer :--> PAsData PScriptContext :--> PUnit)
+alwaysSucceeds = plam $ \datm redm ctx -> pconstant ()
